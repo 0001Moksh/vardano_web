247135bda1c793a432879c11a188bb28f62c6a37
MOKSH BHARDWAJ <164907693+0001Moksh@users.noreply.github.com>
2025-03-15 22:24:05 +0530

    first commit

--- firebase_config.py
+++ firebase_config.py
@@ -1,8 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from dotenv import dotenv_values
+getenv = dotenv_values(".env")
+json_path=getenv.get('json_path')
 
 # Load your Firebase credentials
-cred = credentials.Certificate("json\\vardano-contact-section-firebase-adminsdk-fbsvc-748ca56294.json")
+cred = credentials.Certificate(json_path)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
